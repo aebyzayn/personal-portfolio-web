b6c402345b8cd4e111889d6e7585cb77778ca190
AebyZayn <115506566+aebyzayn@users.noreply.github.com>
2024-01-30 18:58:30 +0600

    Add files via upload

---
+++ data/Data.js
@@ -0,0 +1,137 @@
+
+// Header Data.......................
+import heroImage from '../assets/hero.png'
+
+export const headerData =[
+    {
+        id: 1,
+        img: heroImage,
+        hello:'Hello, Welcome',
+        name: "I'm The AebyZayn.",        
+        info: 'There are many variations of passages ofLorem Ipsum available, but the majority havesuffered alteration in some form, by injected humour,',
+        btnLink: '#Contact'
+    }, 
+];
+
+
+// AboutData Data.......................
+import aboutImage from '../assets/about.png'
+
+export const aboutData =[
+    {
+        id: 1,
+        img: aboutImage, 
+        aboutInfo: 'There are many variations of passages ofLorem Ipsum   available, but the majority havesuffered alteration in some      form, by injected humour,' ,
+        aboutInfo2:'Passages ofLorem Ipsumavailable, but the majority havesuffered alteration in some form, by injected humour,',
+        btnLink: '#Contact'     
+    },
+];
+
+
+// Services Data.......................
+import service1 from '../assets/icon1.png'
+import service2 from '../assets/icon2.png'
+import service3 from '../assets/icon3.png'
+
+
+
+export const servicesData =[
+    {
+        id: 1,
+        img: service1,
+        serviceName: 'Digital Marketing',
+        serviceInfo: 'There are many variations of passages ofLorem Ipsumavailable, but the majority havesuffered alteration in some '        
+    },
+    {
+        id: 2,
+        img: service2, 
+        serviceName: 'Web Devlopment',
+        serviceInfo: 'There are many variations of passages ofLorem Ipsumavailable, but the majority havesuffered alteration in some '        
+    },
+    {
+        id: 3,
+        img: service3,    
+        serviceName: 'Creative Graphic Design',
+        serviceInfo: 'There are many variations of passages ofLorem Ipsumavailable, but the majority havesuffered alteration in some '     
+    },   
+];
+
+
+// Portfolio Data.......................
+import portfolioImg1 from '../assets/p1.png'
+import portfolioImg2 from '../assets/p2.png'
+import portfolioImg3 from '../assets/p3.png'
+import portfolioImg4 from '../assets/p4.png'
+
+
+
+export const portfolioData =[
+    {
+        id: 1,
+        img: portfolioImg1,
+        name: 'App Dashboard',
+        info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ',        
+        btnLink: '#'
+    },
+    {
+        id: 2,
+        img: portfolioImg2,
+        name: 'App Dashboard',
+        info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse',        
+        btnLink: '#'
+    },
+    {
+        id: 3,
+        img: portfolioImg3,
+        name: 'Easy Rent',
+        info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ',        
+        btnLink: '#'
+    },
+    {
+        id: 4,
+        img: portfolioImg4,
+        name: 'Easy Rent',
+        info: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse',        
+        btnLink: '#'
+    },  
+];
+
+
+
+// Experiences Data.......................
+export const experiencesData=[
+    {
+        id: 1,
+        date: 'June-2002 - 2020',
+        serviceName: 'UX/UI Designer',
+        tool: 'Figma',
+        serviceInfo: 'There are many variations of passages of Lorem Ipsumav ailable, but the majority have suffered alteration in some'
+    },
+    {
+        id: 2,
+        date: 'June-2002 - 2020',
+        serviceName: 'Product Designer',
+        tool: 'Adobe Photoshop',
+        serviceInfo: 'There are many variations of passages of Lorem Ipsumav ailable, but the majority have suffered alteration in some'
+    },
+    {
+        id: 3,
+        date: 'June-2002 - 2020',
+        serviceName: 'Graphic Designer',
+        tool: 'Adobe Photoshop',
+        serviceInfo: 'There are many variations of passages of Lorem Ipsumav ailable, but the majority have suffered alteration in some'
+    }
+];
+
+
+
+ // Contact Data .......................
+ export const contactData = [
+  {
+    id: 1,
+    address: '23 S 80 Road, Benedict,ne, 68316',
+    phone: ' +01 123 456 789', 
+    email: 'abc.xyz@gmail.com',    
+    web: 'www.abc.xyz.com',    
+  },
+ ];
